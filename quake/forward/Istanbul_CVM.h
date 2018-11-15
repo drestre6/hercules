3144c46931d4eb4850cefeb28be9f925e5d93096
Dorian Restrepo <drestre6@gmail.com>
2018-11-15 15:56:39 -0500

    Removed iterpolation functions from C library

--- quake/forward/Istanbul_CVM.h
+++ quake/forward/Istanbul_CVM.h
@@ -11,21 +11,7 @@
 
 /* ===================  pwl_interp_2d_scattered  ===================  */
 
-double *pwl_interp_2d_scattered_value ( int nd, double xyd[], double zd[], int t_num, int t[], int t_neighbor[], int ni, double xyi[] );
-
-void triangulation_search_delaunay ( int node_num, double node_xy[],
-  int triangle_order, int triangle_num, int triangle_node[],
-  int triangle_neighbor[], double p[2], int *triangle_index,
-  double *alpha, double *beta, double *gamma, int *edge,
-  int *step_num );
-
-
-void material_property_relative_V6(double x_input, double y_input, double z_input, double output[3] );
-
-void material_property_relative_V444S(double x_input, double y_input, double z_input, double output[3] ) ;
-void material_property_relative_V444S_nointerp(double x_input, double y_input, double z_input, double output[3] ) ;
-
-void material_property_relative_V9(double x_input, double y_input, double z_input, double output[3] ) ;
+void material_property_relative_V10(double x_input, double y_input, double z_input, double output[3] ) ;
 
 
 #endif /* ISTANBUL_CVM_H_ */
